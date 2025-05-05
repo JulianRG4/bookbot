59f1ea9f39a1b4e041c50dea0e661ff54319ebe9
JulianRG4 <Juliang4209@gmail.com>
2025-05-05 13:33:14 -0700

    removed hard filepath and replaced it with sys.argv

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import get_book_text, count_book_words, count_book_char, sorted_char_dictionary
+import sys
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
     file_text = get_book_text(file_path)
     num_words = count_book_words(file_text)
     num_char = count_book_char(file_text)
